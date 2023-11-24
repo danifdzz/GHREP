9ffe44d2a6446da6dd7ef300abfb05a0004ede21
Alumno Presencial <Alumno Presencial@DESKTOP-B44B142>
2023-11-24 09:23:03 +0100

    tercer commit(variables)

--- Ejemplo_git/src/main/java/principal.java
+++ Ejemplo_git/src/main/java/principal.java
@@ -4,7 +4,8 @@ public class principal {
 	public static void main(String[] args) {
 		System.out.println("Hola GIT ");
 		System.out.println("Ya se usar GIT");
-		
+		String a="pelayo";
+		int num= 5656;
 		
 	}
 
