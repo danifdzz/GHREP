51fbe63cd9582149658b0beb041b19893e978ce6
danifdzz <151909953+danifdzz@users.noreply.github.com>
2023-11-27 11:09:14 +0100

    Update principal.java
    Modificacion desde github

--- Ejemplo_git/src/main/java/principal.java
+++ Ejemplo_git/src/main/java/principal.java
@@ -4,10 +4,10 @@ public class principal {
 	public static void main(String[] args) {
 		System.out.println("Hola GIT ");
 		System.out.println("Ya se usar GIT");
-		String a="pelayo";
+		
 		int num= 5656;
 		System.out.println("ahota en github");
-		
+		String variable = "String definida en github";
 	}
 
 }
